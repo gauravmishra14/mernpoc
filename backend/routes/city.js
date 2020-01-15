1b7c55092911f4f6b628cce3e4065de6f27ac4bc
Gaurav Mishra <gauravdmishra14@gmail.com>
2020-01-15 19:59:44 +0530

    logic update

--- backend/routes/city.js
+++ backend/routes/city.js
@@ -1,28 +1,27 @@
-const router = require('express').Router();
-let City = require('../models/city.model');
-let cityApi = require('../external_api/index');
+const router = require("express").Router();
+let City = require("../models/city.model");
+let cityApi = require("../external_api/index");
 
+let capitalCaseStates = ["Sunnyvale", "Chicago", "Dallas"];
+let smallCaseStates = ["sunnyvale", "chicago", "dallas"];
 
+router.route("/").get((req, res) => {
+  var inputCity = req.query.city;
+  var newChar;
+  if (inputCity === smallCaseStates[0]) {
+    newChar = capitalCaseStates[0];
+  } else if (inputCity === smallCaseStates[1]) {
+    newChar = capitalCaseStates[1];
+  } else {
+    newChar = capitalCaseStates[2];
+  }
 
-var states = ['Sunnyvale','Chicago','Dallas']
-
-
-
-
-
-router.route('/').get((req, res) => {
-    var param = req.query.city;
-    param = param.trim();
-    var newCity;
-    if(param === "sunnyvale"){
-        console.log('inside if')
-        newCity = 'Sunnyvale'
-    }
-    console.log('param: '+param)
-    console.log('newCity: '+newCity)
-    // City.find()
-    // .then(cities => {const a = cities.filter(city => city.location.city === newChar); a.length ? res.json(a) : cityApi(inputCity)})
-    // .catch(err => res.status(400).json('Error: '+err));
+  City.find()
+    .then(cities => {
+      const a = cities.filter(city => city.location.city === newChar);
+      a.length ? res.json(a) : cityApi(inputCity);
+    })
+    .catch(err => res.status(400).json("Error: " + err));
 });
 
 // Object.entries(obj).length === 0 && obj.constructor === Object)
